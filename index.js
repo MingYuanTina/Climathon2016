9c53d4ff5e1bb7ba19c60b7cff47b758fc6ede26
Julian Maingot <jrmaingo@uwaterloo.ca>
2016-10-29 00:17:34 -0400

    Added basic api call

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.use(express.static(__dirname + '/'));
 app.use(express.static(__dirname + '/node_modules/'));
 app.use(express.static(__dirname + '/app/'));
 
+// link to DB (currently not implemented)
+var db = {
+    validateUser: function(userName, password) {
+        // TODO query DB for validation
+        return true;
+    }
+};
+
+// checks DB to validate userName and password
+app.get("/api/login", function(req, res) {
+    res.json(db.validateUser(req.userName, req.password));
+});
+
+// app hosted on localhost:3000
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
