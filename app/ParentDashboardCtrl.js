8ea9d137356c22cd9ebde51ceb28552dad07476a
JLIVE101 <jwilliams199055@gmail.com>
2016-10-29 07:50:18 -0400

    added autocomplete and fixed directions

--- app/ParentDashboardCtrl.js
+++ app/ParentDashboardCtrl.js
@@ -9,6 +9,17 @@ function($scope, DropOffLocnService) {
         latLng: [5, 7]
     };
 
+    $scope.origin = "";
+    $scope.destination = "";
+    $scope.schoolLoc = [43.7286572,-79.5685916];
+    $scope.locArr = [
+     [43.730849, -79.577471],
+     [43.725918, -79.566472],
+     [43.726158, -79.561762]
+   ];
+
+    console.log($scope.locArr);
+
     var dest = []; // stores selected destination
     var routes = []; // stores the available routes
     var selectedRoute = {}; // stores the currently selected route
@@ -17,9 +28,31 @@ function($scope, DropOffLocnService) {
         // needs to fetch new routelist from backend
     }
 
+    $scope.mycallback = function(map) {
+        $scope.mymap = map;
+        $scope.$apply();
+    };
+
+
+
+    $scope.fromChanged = function() {
+        //console.log(this.getPlace().geometry.location);
+        $scope.origin = this.getPlace().geometry.location;
+        $scope.$apply();
+    };
+
+    $scope.toChanged = function() {
+      //console.log(this.getPlace().geometry.location);
+      $scope.destination = this.getPlace().geometry.location;
+      $scope.$apply();
+    };
+
     $scope.dropOffLocns = DropOffLocnService.getDropOffLocns();
-    $scope.selectDest = function(e, dest) {
-        $scope.dest = dest;
-        updateRouteList();
+
+    $scope.selectDest = function(e) {
+        $scope.origin = e.latLng;
+        $scope.destination = $scope.schoolLoc;
+        $scope.$apply();
+
     };
 }]);
